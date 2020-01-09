6823b92dfbec82a697efd8d0dcdf8bd644241afe
Alexii Aidinov <alexii.aidinov@gmail.com>
2020-01-09 20:00:51 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>List of users</h1>
+    </div>
+  );
+};
+
+export default App;

25416b172c2262206969d96f9490b749ee6c6cec
Alexii Aidinov <alexii.aidinov@gmail.com>
2020-01-10 20:08:59 +0200

    change font-family of titles

--- build/precache-manifest.a4e3798fe98edad1068d2c0235da7950.js
+++ build/precache-manifest.a4e3798fe98edad1068d2c0235da7950.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c80da97161ecbd429e1848a701466e9",
+    "revision": "2762381d0f7860eb514ac8fa8fb1b63b",
     "url": "/react_list-of-users/index.html"
   },
   {
-    "revision": "170bb97c3bbe679f5867",
+    "revision": "47171e6e4a9865430ea4",
     "url": "/react_list-of-users/static/css/main.76748582.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_list-of-users/static/js/2.57afee1c.chunk.js"
   },
   {
-    "revision": "170bb97c3bbe679f5867",
-    "url": "/react_list-of-users/static/js/main.34468e6e.chunk.js"
+    "revision": "47171e6e4a9865430ea4",
+    "url": "/react_list-of-users/static/js/main.49e9b062.chunk.js"
   },
   {
     "revision": "260f675403606f5a7554",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-users/precache-manifest.54eac1d13207031011cc570a7a3ef232.js"
+  "/react_list-of-users/precache-manifest.a4e3798fe98edad1068d2c0235da7950.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_list-of-users/index.html"), {
-
+  
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

e2ffcbc0fbae50dedee748e49fe1f6841d1b9b64
Alexii Aidinov <alexii.aidinov@gmail.com>
2020-01-10 21:13:16 +0200

    Updates

---
+++ precache-manifest.9cd09872e7ecfcbf1afddbc6e3e8ce2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e210d7a0b9ed5f0044c6a7d58e693bcf",
+    "url": "/react_list-of-users/index.html"
+  },
+  {
+    "revision": "52d35644c19ae7e2ab36",
+    "url": "/react_list-of-users/static/css/main.1545681d.chunk.css"
+  },
+  {
+    "revision": "3cc7e274c7d14679a07d",
+    "url": "/react_list-of-users/static/js/2.57afee1c.chunk.js"
+  },
+  {
+    "revision": "52d35644c19ae7e2ab36",
+    "url": "/react_list-of-users/static/js/main.49e9b062.chunk.js"
+  },
+  {
+    "revision": "260f675403606f5a7554",
+    "url": "/react_list-of-users/static/js/runtime~main.e33389ca.js"
+  }
+]);
